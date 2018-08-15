4c5d1200bcaa072773a459f03c8916e568aecbf5
Parth <parth647@gmail.com>
2018-08-16 01:18:22 +0530

    added more analysis

--- main.py
+++ main.py
@@ -79,3 +79,31 @@ facet.add_legend()
 fig, axis1 = plt.subplots(1,1,figsize=(18,4))
 average_age = titanic_df[["Age", "Survived"]].groupby(['Age'],as_index=False).mean()
 sns.barplot(x='Age', y='Survived', data=average_age)
+# Family
+
+# Instead of having two columns Parch & SibSp, 
+# we can have only one column represent if the passenger had any family member aboard or not,
+# Meaning, if having any family member(whether parent, brother, ...etc) will increase chances of Survival or not.
+titanic_df['Family'] =  titanic_df["Parch"] + titanic_df["SibSp"]
+titanic_df['Family'].loc[titanic_df['Family'] > 0] = 1
+titanic_df['Family'].loc[titanic_df['Family'] == 0] = 0
+
+test_df['Family'] =  test_df["Parch"] + test_df["SibSp"]
+test_df['Family'].loc[test_df['Family'] > 0] = 1
+test_df['Family'].loc[test_df['Family'] == 0] = 0
+
+# drop Parch & SibSp
+titanic_df = titanic_df.drop(['SibSp','Parch'], axis=1)
+test_df    = test_df.drop(['SibSp','Parch'], axis=1)
+
+# plot
+fig, (axis1,axis2) = plt.subplots(1,2,sharex=True,figsize=(10,5))
+
+# sns.factorplot('Family',data=titanic_df,kind='count',ax=axis1)
+sns.countplot(x='Family', data=titanic_df, order=[1,0], ax=axis1)
+
+# average of survived for those who had/didn't have any family member
+family_perc = titanic_df[["Family", "Survived"]].groupby(['Family'],as_index=False).mean()
+sns.barplot(x='Family', y='Survived', data=family_perc, order=[1,0], ax=axis2)
+
+axis1.set_xticklabels(["With Family","Alone"], rotation=0)
--- trial_titanic.py
+++ trial_titanic.py
@@ -25,3 +25,46 @@ sns.countplot(x='Embarked', data=titanic_df, ax=axis1)
 sns.countplot(x='Survived', hue="Embarked", data=titanic_df, order=[1,0], ax=axis2)
 embark_perc = titanic_df[["Embarked", "Survived"]].groupby(['Embarked'],as_index=False).mean()
 sns.barplot(x='Embarked', y='Survived', data=embark_perc,order=['S','C','Q'],ax=axis3)
+#Embarked will not contribute significantly to the predictions hence we will drop this column
+# group by embarked, and get the mean for survived passengers for each value in Embarked
+embark_perc = titanic_df[["Embarked", "Survived"]].groupby(['Embarked'],as_index=False).mean()
+sns.barplot(x='Embarked', y='Survived', data=embark_perc,order=['S','C','Q'],ax=axis3)
+
+# Either to consider Embarked column in predictions,
+# and remove "S" dummy variable, 
+# and leave "C" & "Q", since they seem to have a good rate for Survival.
+
+# OR, don't create dummy variables for Embarked column, just drop it, 
+# because logically, Embarked doesn't seem to be useful in prediction.
+
+embark_dummies_titanic  = pd.get_dummies(titanic_df['Embarked'])
+embark_dummies_titanic.drop(['S'], axis=1, inplace=True)
+
+embark_dummies_test  = pd.get_dummies(test_df['Embarked'])
+embark_dummies_test.drop(['S'], axis=1, inplace=True)
+
+titanic_df = titanic_df.join(embark_dummies_titanic)
+test_df    = test_df.join(embark_dummies_test)
+
+titanic_df.drop(['Embarked'], axis=1,inplace=True)
+test_df.drop(['Embarked'], axis=1,inplace=True)
+# only for test_df, since there is a missing "Fare" values
+test_df["Fare"].fillna(test_df["Fare"].median(), inplace=True)
+
+# convert from float to int
+titanic_df['Fare'] = titanic_df['Fare'].astype(int)
+test_df['Fare']    = test_df['Fare'].astype(int)
+
+# get fare for survived & didn't survive passengers 
+fare_not_survived = titanic_df["Fare"][titanic_df["Survived"] == 0]
+fare_survived     = titanic_df["Fare"][titanic_df["Survived"] == 1]
+
+# get average and std for fare of survived/not survived passengers
+avgerage_fare = DataFrame([fare_not_survived.mean(), fare_survived.mean()])
+std_fare      = DataFrame([fare_not_survived.std(), fare_survived.std()])
+
+# plot
+titanic_df['Fare'].plot(kind='hist', figsize=(20,5),bins=100, xlim=(0,50))
+
+avgerage_fare.index.names = std_fare.index.names = ["Survived"]
+avgerage_fare.plot(yerr=std_fare,kind='bar',legend=False)
