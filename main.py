5064c35ddfd618505264c43418d16eae3b5cc1bd
Parth <parth647@gmail.com>
2018-10-03 00:57:14 +0530

    updated

--- main.py
+++ main.py
@@ -107,3 +107,37 @@ family_perc = titanic_df[["Family", "Survived"]].groupby(['Family'],as_index=Fal
 sns.barplot(x='Family', y='Survived', data=family_perc, order=[1,0], ax=axis2)
 
 axis1.set_xticklabels(["With Family","Alone"], rotation=0)
+def get_person(passenger):
+    age,sex = passenger
+    return 'child' if age < 16 else sex
+    
+titanic_df['Person'] = titanic_df[['Age','Sex']].apply(get_person,axis=1)
+test_df['Person']    = test_df[['Age','Sex']].apply(get_person,axis=1)
+
+# No need to use Sex column since we created Person column
+titanic_df.drop(['Sex'],axis=1,inplace=True)
+test_df.drop(['Sex'],axis=1,inplace=True)
+
+# create dummy variables for Person column, & drop Male as it has the lowest average of survived passengers
+person_dummies_titanic  = pd.get_dummies(titanic_df['Person'])
+person_dummies_titanic.columns = ['Child','Female','Male']
+person_dummies_titanic.drop(['Male'], axis=1, inplace=True)
+
+person_dummies_test  = pd.get_dummies(test_df['Person'])
+person_dummies_test.columns = ['Child','Female','Male']
+person_dummies_test.drop(['Male'], axis=1, inplace=True)
+
+titanic_df = titanic_df.join(person_dummies_titanic)
+test_df    = test_df.join(person_dummies_test)
+
+fig, (axis1,axis2) = plt.subplots(1,2,figsize=(10,5))
+
+# sns.factorplot('Person',data=titanic_df,kind='count',ax=axis1)
+sns.countplot(x='Person', data=titanic_df, ax=axis1)
+
+# average of survived for each Person(male, female, or child)
+person_perc = titanic_df[["Person", "Survived"]].groupby(['Person'],as_index=False).mean()
+sns.barplot(x='Person', y='Survived', data=person_perc, ax=axis2, order=['male','female','child'])
+
+titanic_df.drop(['Person'],axis=1,inplace=True)
+test_df.drop(['Person'],axis=1,inplace=True)
